bc8c2cedb3e81716fd523cdfb6a19277049f5e01
Ahmed Mamdouh <ahmed.mamdouh.elattar@gmail.com>
2024-10-17 15:37:16 +0300

    add assets and generate assets file

---
+++ lib/core/configs/assets.dart
@@ -0,0 +1,97 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesDiscoveryDark
+  /// assets/images/discovery_dark.svg
+  static const String imagesDiscoveryDark = "assets/images/discovery_dark.svg";
+
+  /// Assets for imagesDiscoveryLight
+  /// assets/images/discovery_light.svg
+  static const String imagesDiscoveryLight = "assets/images/discovery_light.svg";
+
+  /// Assets for imagesFavouriteDark
+  /// assets/images/favourite_dark.svg
+  static const String imagesFavouriteDark = "assets/images/favourite_dark.svg";
+
+  /// Assets for imagesFavouriteLight
+  /// assets/images/favourite_light.svg
+  static const String imagesFavouriteLight = "assets/images/favourite_light.svg";
+
+  /// Assets for imagesHeartDark
+  /// assets/images/heart_dark.svg
+  static const String imagesHeartDark = "assets/images/heart_dark.svg";
+
+  /// Assets for imagesHeartLight
+  /// assets/images/heart_light.svg
+  static const String imagesHeartLight = "assets/images/heart_light.svg";
+
+  /// Assets for imagesHomeDark
+  /// assets/images/home_dark.svg
+  static const String imagesHomeDark = "assets/images/home_dark.svg";
+
+  /// Assets for imagesHomeLight
+  /// assets/images/home_light.svg
+  static const String imagesHomeLight = "assets/images/home_light.svg";
+
+  /// Assets for imagesMoon
+  /// assets/images/moon.svg
+  static const String imagesMoon = "assets/images/moon.svg";
+
+  /// Assets for imagesNextDark
+  /// assets/images/next_dark.svg
+  static const String imagesNextDark = "assets/images/next_dark.svg";
+
+  /// Assets for imagesNextLight
+  /// assets/images/next_light.svg
+  static const String imagesNextLight = "assets/images/next_light.svg";
+
+  /// Assets for imagesPause
+  /// assets/images/pause.svg
+  static const String imagesPause = "assets/images/pause.svg";
+
+  /// Assets for imagesPlayDark
+  /// assets/images/play_dark.svg
+  static const String imagesPlayDark = "assets/images/play_dark.svg";
+
+  /// Assets for imagesPlayLight
+  /// assets/images/play_light.svg
+  static const String imagesPlayLight = "assets/images/play_light.svg";
+
+  /// Assets for imagesPreviousDark
+  /// assets/images/previous_dark.svg
+  static const String imagesPreviousDark = "assets/images/previous_dark.svg";
+
+  /// Assets for imagesPreviousLight
+  /// assets/images/previous_light.svg
+  static const String imagesPreviousLight = "assets/images/previous_light.svg";
+
+  /// Assets for imagesProfileDark
+  /// assets/images/profile_dark.svg
+  static const String imagesProfileDark = "assets/images/profile_dark.svg";
+
+  /// Assets for imagesProfileLight
+  /// assets/images/profile_light.svg
+  static const String imagesProfileLight = "assets/images/profile_light.svg";
+
+  /// Assets for imagesRepeatDark
+  /// assets/images/repeat_dark.svg
+  static const String imagesRepeatDark = "assets/images/repeat_dark.svg";
+
+  /// Assets for imagesRepeatLight
+  /// assets/images/repeat_light.svg
+  static const String imagesRepeatLight = "assets/images/repeat_light.svg";
+
+  /// Assets for imagesShuffleDark
+  /// assets/images/shuffle_dark.svg
+  static const String imagesShuffleDark = "assets/images/shuffle_dark.svg";
+
+  /// Assets for imagesShuffleLight
+  /// assets/images/shuffle_light.svg
+  static const String imagesShuffleLight = "assets/images/shuffle_light.svg";
+
+  /// Assets for imagesSun
+  /// assets/images/sun.svg
+  static const String imagesSun = "assets/images/sun.svg";
+}
+
