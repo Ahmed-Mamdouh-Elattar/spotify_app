963a53b6b9936774c2c6516d903cf2c7f584138d
Ahmed Mamdouh <ahmed.mamdouh.elattar@gmail.com>
2024-10-17 15:11:18 +0300

    initiate app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SpotifyApp());
+}
+
+class SpotifyApp extends StatelessWidget {
+  const SpotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
